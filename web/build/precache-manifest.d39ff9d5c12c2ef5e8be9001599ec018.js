e9b20be046a135b12c82415ae56deeed6942de32
RafaelFantinel <rafaeldeoliveirafantinel@hotmail.com>
2020-10-16 16:09:41 -0300

    Finalizado Front-End, Back-End e Mobile.

---
+++ web/build/precache-manifest.d39ff9d5c12c2ef5e8be9001599ec018.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ef80b21701403596ee232ed701194f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f09423ed0f77bcb5e9eb",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "1e9745e099bd3608a2ec",
+    "url": "/static/css/main.2086611b.chunk.css"
+  },
+  {
+    "revision": "f09423ed0f77bcb5e9eb",
+    "url": "/static/js/2.918b491d.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/static/js/2.918b491d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9745e099bd3608a2ec",
+    "url": "/static/js/main.009c01b8.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/static/media/map-marker.658b8302.svg"
+  }
+]);
